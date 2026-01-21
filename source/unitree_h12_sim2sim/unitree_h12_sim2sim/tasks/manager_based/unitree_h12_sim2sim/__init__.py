8dfc4f8424bba04dcf0fd8d8190896c7032aafda
Niraj Pudasaini <np2289@nyu.edu>
2026-01-20 22:23:17 -0700

    Initial commit

---
+++ source/unitree_h12_sim2sim/unitree_h12_sim2sim/tasks/manager_based/unitree_h12_sim2sim/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+
+gym.register(
+    id="Template-Unitree-H12-Sim2sim-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.unitree_h12_sim2sim_env_cfg:UnitreeH12Sim2simEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
+    },
+)
